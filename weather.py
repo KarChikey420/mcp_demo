27b2882e7b0e7b30f90a0f6adb8bb1d9a92a36c3
KarChikey <kartikeynegi2002@gmail.com>
2026-02-11 01:10:33 +0530

    aff

--- weather.py
+++ weather.py
@@ -1,3 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("math-server")
+
+@mcp.tool()
+def weather(city: str) -> str:
+    """Get the weather for a city."""
+    return f"The weather is sunny."
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
